d9561494c11291dfffddd06201d355ba965f8928
Kojo <lbwiththecodes@gmail.com>
2025-09-21 18:50:21 -0700

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      WELCOME TO BYTECREST INC
+
+      <Button>GREAT 👍</Button>
+    </>
+  )
+}
+
+export default App
